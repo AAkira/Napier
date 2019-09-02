98dd5f443f046238e55db3ff6a126078ae02f7ef
AAkira <developer.a.akira@gmail.com>
2019-09-02 20:16:23 +0900

    Fix jvm imports.

--- jvm/src/main/kotlin/com/github/aakira/napier/sample/Main.kt
+++ jvm/src/main/kotlin/com/github/aakira/napier/sample/Main.kt
@@ -2,7 +2,7 @@ package com.github.aakira.napier.sample
 
 import com.github.aakira.napier.DebugAntilog
 import com.github.aakira.napier.Napier
-import com.github.aakira.napier.Sample
+import com.github.aakira.napier.mppsample.Sample
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
