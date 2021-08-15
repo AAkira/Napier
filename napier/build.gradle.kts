4bcb4e24589acb8c1881c99ac1843afa52de687b
AAkira <developer.a.akira@gmail.com>
2021-08-16 02:31:14 +0900

    Add macOS in Napier.

--- napier/build.gradle.kts
+++ napier/build.gradle.kts
@@ -8,153 +8,28 @@ plugins {
 
 apply(from = rootProject.file("./gradle/publish.gradle.kts"))
 
-enum class HostOs {
-    LINUX, WINDOWS, MAC
-}
-
-val napierPrimaryDevelopmentOs: String? by project
-
-val primaryDevelopmentOs: HostOs = if (napierPrimaryDevelopmentOs != null) {
-    println("Selected dev OS: $napierPrimaryDevelopmentOs")
-    when (napierPrimaryDevelopmentOs) {
-        "linux" -> HostOs.LINUX
-        "windows" -> HostOs.WINDOWS
-        "mac" -> HostOs.MAC
-        else -> throw GradleException(
-            "Invalid development environment OS select: " +
-                    "$napierPrimaryDevelopmentOs. Only linux, windows and mac are supported at the moment"
-        )
-    }
-} else {
-    HostOs.LINUX
-}
-
 val ideaActive = System.getProperty("idea.active") == "true"
 
-fun getHostOsName(): HostOs {
-    val target = System.getProperty("os.name")
-    if (target == "Linux") return HostOs.LINUX
-    if (target.startsWith("Windows")) return HostOs.WINDOWS
-    if (target.startsWith("Mac")) return HostOs.MAC
-    throw GradleException("Unknown OS: $target")
-}
-
 kotlin {
-    val hostOs = getHostOsName()
-    println("Host os name $hostOs")
-
-    if (ideaActive) {
-        when (hostOs) {
-            HostOs.LINUX -> linuxX64("native")
-            HostOs.MAC -> macosX64("native")
-            HostOs.WINDOWS -> mingwX64("native")
-        }
-    }
-
     android {
         publishAllLibraryVariants()
     }
-//    ios {
-//        binaries {
-//            framework()
-//        }
-//    }
     js {
         browser()
         nodejs()
     }
     jvm()
 
-    if (hostOs == HostOs.LINUX) {
-
-        linuxX64("linux") {
-            binaries {
-                staticLib {
-                }
-            }
-        }
-
-        if (ideaActive.not()) {
-            linuxArm32Hfp {
-                binaries {
-                    staticLib {
-                    }
-                }
-            }
-
-            linuxArm64 {
-                binaries {
-                    staticLib {
-                    }
-                }
-            }
-        }
-    }
-
-    iosX64("ios") {
+    ios {
         binaries {
-            framework {
-            }
-        }
-    }
-    iosArm64("ios64Arm") {
-        binaries {
-            framework {
-            }
-        }
-    }
-
-    iosArm32("ios32Arm") {
-        binaries {
-            framework {
-            }
+            framework()
         }
     }
     macosX64 {
         binaries {
-            framework {
-            }
+            framework()
         }
     }
-
-    tvos {
-        binaries {
-            framework {
-            }
-        }
-    }
-    if (ideaActive.not()) {
-        watchos {
-            binaries {
-                framework {
-                }
-            }
-        }
-    }
-
-    watchosX86 {
-        binaries {
-            framework {
-            }
-        }
-    }
-
-    mingwX64 {
-        binaries {
-            staticLib {
-            }
-        }
-    }
-    if (ideaActive.not()) {
-        mingwX86 {
-            binaries {
-                staticLib {
-                }
-            }
-        }
-    }
-
-    println(targets.names)
 
     sourceSets {
         val commonMain by getting {
@@ -178,36 +53,26 @@ kotlin {
                 implementation(Dep.Test.jvm)
             }
         }
-//        val iosMain by getting {
-//            dependencies {
-//            }
-//        }
-//        val iosTest by getting {
-//            dependencies {
-//            }
-//        }
 
-        if (hostOs == primaryDevelopmentOs) {
-            val jsMain by getting {
-                dependencies {
-                    implementation(Dep.Kotlin.js)
-                }
+        val jsMain by getting {
+            dependencies {
+                implementation(Dep.Kotlin.js)
             }
-            val jsTest by getting {
-                dependencies {
-                    implementation(Dep.Test.js)
-                }
+        }
+        val jsTest by getting {
+            dependencies {
+                implementation(Dep.Test.js)
             }
-            val jvmMain by getting {
-                dependencies {
-                    implementation(Dep.Kotlin.jvm)
-                }
+        }
+        val jvmMain by getting {
+            dependencies {
+                implementation(Dep.Kotlin.jvm)
             }
-            val jvmTest by getting {
-                dependencies {
-                    implementation(Dep.Coroutines.core)
-                    implementation(Dep.Test.jvm)
-                }
+        }
+        val jvmTest by getting {
+            dependencies {
+                implementation(Dep.Coroutines.core)
+                implementation(Dep.Test.jvm)
             }
         }
 
@@ -234,53 +99,13 @@ kotlin {
             nativeTest
         }
 
-        if (hostOs == HostOs.LINUX) {
-
-            val linuxMain by getting {
-                dependsOn(nativeMain)
-            }
-            val linuxTest by getting {
-                dependsOn(nativeTest)
-            }
-
-            if (ideaActive.not()) {
-
-                val linuxArm32HfpMain by getting {
-                    dependsOn(nativeMain)
-                }
-
-                val linuxArm32HfpTest by getting {
-                    dependsOn(nativeTest)
-                }
-
-                val linuxArm64Main by getting {
-                    dependsOn(nativeMain)
-                }
-
-                val linuxArm64Test by getting {
-                    dependsOn(nativeTest)
-                }
-            }
-        }
         val iosMain by getting {
-            dependsOn(nativeMain)
+            dependencies {
+            }
         }
         val iosTest by getting {
-            dependsOn(nativeTest)
-        }
-
-        val ios64ArmMain by getting {
-            dependsOn(nativeMain)
-        }
-        val ios64ArmTest by getting {
-            dependsOn(nativeTest)
-        }
-
-        val ios32ArmMain by getting {
-            dependsOn(nativeMain)
-        }
-        val ios32ArmTest by getting {
-            dependsOn(nativeTest)
+            dependencies {
+            }
         }
 
         val macosX64Main by getting {
@@ -289,49 +114,6 @@ kotlin {
         val macosX64Test by getting {
             dependsOn(nativeTest)
         }
-
-        val tvosMain by getting {
-            dependsOn(nativeMain)
-        }
-        val tvosTest by getting {
-            dependsOn(nativeTest)
-        }
-        if (ideaActive.not()) {
-            val watchosMain by getting {
-                dependsOn(nativeMain)
-            }
-
-            val watchosTest by getting {
-                dependsOn(nativeTest)
-            }
-        }
-
-        val watchosX86Main by getting {
-            dependsOn(nativeMain)
-        }
-
-        val watchosX86Test by getting {
-            dependsOn(nativeTest)
-        }
-
-        val mingwX64Main by getting {
-            dependsOn(nativeMain)
-        }
-
-        val mingwX64Test by getting {
-            dependsOn(nativeTest)
-        }
-
-        if (ideaActive.not()) {
-            val mingwX86Main by getting {
-                dependsOn(nativeMain)
-            }
-
-            val mingwX86Test by getting {
-                dependsOn(nativeTest)
-            }
-        }
-
     }
 }
 
